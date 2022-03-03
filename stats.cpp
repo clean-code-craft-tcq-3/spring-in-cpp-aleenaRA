a94db18912e2b0f2349d42e53119b809ebd1ab64
aleenaRA <99877348+aleenaRA@users.noreply.github.com>
2022-03-03 14:57:23 +0530

    stats.cpp average

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,12 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& values) {
     //Implement statistics here
+    Stats obj;
+    for(int index = 0, float sum=0.0 ; index < values.size() ; index++)
+        sum += values[index];
+    obj.average = sum / values.size();
+    
+    return obj;
+        
 }
