d70bae1715648256aa6e6edf2d6e9f3b589ad53c
aleenaRA <99877348+aleenaRA@users.noreply.github.com>
2022-03-03 15:51:15 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,10 @@
 #include <vector>
 
 namespace Statistics {
+    
+    class Stats{
+        public:
+        float average, min , max;
+    };
     Stats ComputeStatistics(const std::vector<float>& );
 }
