6d3fabc439d451886367ea82daa3bc481e64d1a1
aleenaRA <99877348+aleenaRA@users.noreply.github.com>
2022-03-03 15:41:17 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,10 +3,17 @@
 Stats Statistics::ComputeStatistics(const std::vector<float>& values) {
     //Implement statistics here
     Stats obj;
+    float obj.max = values.front();
+    float obj.min = values.front();
     for(int index = 0, float sum=0.0 ; index < values.size() ; index++)
+    {    
         sum += values[index];
+        if(obj.max < values[index])
+            obj.max = values[index];
+        if(obj.min > values[index])
+            obj.min = values[index];
+    }   
     obj.average = sum / values.size();
     
-    return obj;
-        
+    return obj;       
 }
