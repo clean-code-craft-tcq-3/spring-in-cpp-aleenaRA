2f5d0ff4bafee6ea6dfef4a6d8ab93149dfcafe8
aleenaRA <99877348+aleenaRA@users.noreply.github.com>
2022-03-04 06:41:12 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -13,24 +13,28 @@ TEST_CASE("reports average, minimum and maximum") {
     REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
 
-// TEST_CASE("average is NaN for empty array") {
-//     auto computedStats = Statistics::ComputeStatistics({});
-//     //All fields of computedStats (average, max, min) must be
-//     //NAN (not-a-number), as defined in math.h
+TEST_CASE("average is NaN for empty array") {
+    auto computedStats = Statistics::ComputeStatistics({});
+    //All fields of computedStats (average, max, min) must be
+    //NAN (not-a-number), as defined in math.h
     
-//     //Design the REQUIRE statement here.
-//     //Use http://www.cplusplus.com/reference/cmath/isnan/
-// }
-
-// TEST_CASE("raises alerts when max is greater than threshold") {
-//     EmailAlert emailAlert;
-//     LEDAlert ledAlert;
-//     std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
+    //Design the REQUIRE statement here.
+    //Use http://www.cplusplus.com/reference/cmath/isnan/
     
-//     const float maxThreshold = 10.2;
-//     StatsAlerter statsAlerter(maxThreshold, alerters);
-//     statsAlerter.checkAndAlert({99.8, 34.2, 4.5, 6.7});
+    REQUIRE((isnan(computedStats.average)) == 1);
+    REQUIRE((isnan(computedStats.max)) == 1);
+    REQUIRE((isnan(computedStats.min)) == 1);
+}
 
-//     REQUIRE(emailAlert.emailSent);
-//     REQUIRE(ledAlert.ledGlows);
-// }
+TEST_CASE("raises alerts when max is greater than threshold") {
+    EmailAlert emailAlert;
+    LEDAlert ledAlert;
+    std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
+    
+    const float maxThreshold = 10.2;
+    StatsAlerter statsAlerter(maxThreshold, alerters);
+    statsAlerter.checkAndAlert({99.8, 34.2, 4.5, 6.7});
+
+    REQUIRE(emailAlert.emailSent);
+    REQUIRE(ledAlert.ledGlows);
+}
